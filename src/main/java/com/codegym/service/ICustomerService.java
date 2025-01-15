77230f94f61fb9dc9da9970f97886a146af92edb
vuong1995hd <hoangvietvuong25@gmail.com>
2025-01-15 09:40:37 +0700

    TH

---
+++ src/main/java/com/codegym/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
+}
